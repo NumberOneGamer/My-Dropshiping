d635472ba436c93d01c77bd567d572355958094b
Dropship Deploy <deploy@dropship.com>
2026-05-20 19:38:36 +0300

    fix: remove broken import in admin-float-btn.tsx

--- components/admin/admin-float-btn.tsx
+++ components/admin/admin-float-btn.tsx
@@ -2,7 +2,6 @@
 
 import { useSession } from "next-auth/react";
 import Link from "next/link";
-import { Services } from "./services";
 
 export function ClientAdminCheck() {
   const { data: session } = useSession();
